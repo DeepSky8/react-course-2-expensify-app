be25e269058fdd2a7f8bd9f2dd808b0499212c0d
Joel Jackson <deepsky8@gmail.com>
2022-02-21 16:55:09 -0500

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { addExpense, removeExpense, editExpense } from './actions/expenses';
+// import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
